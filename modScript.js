9a77f7bb93f8d52f2bb16b54384be091bd4962b3
Ashutosh <108ashutosh0@gmail.com>
2026-04-01 21:22:56 +0530

    added delete functionality to delete items

--- modScript.js
+++ modScript.js
@@ -6,6 +6,7 @@ function createNewElements(ntask) {
     const finishDate = document.createElement("span");
     const importance = document.createElement("span");
     const finishSatus = document.createElement("input")
+    const deleteTask = document.createElement("img")
 
     //This is used to add attributes to the check box element Aka finishSatus
     const attrinbutes = {
@@ -15,6 +16,17 @@ function createNewElements(ntask) {
     Object.entries(attrinbutes).forEach(([key, value]) => {
         finishSatus.setAttribute(key, value)
 
+    })
+    const delTaskAttributes = {
+        'class': 'delete_btn',
+        'src': 'icons/delete_btn.png',
+        'alt': 'deleteIcon',
+        'width': '18px',
+        'data-id': `${ntask.taskId}`
+
+    }
+    Object.entries(delTaskAttributes).forEach(([key, value]) => {
+        deleteTask.setAttribute(key, value)
     })
 
     //this code set attributes to the elements created
@@ -26,13 +38,14 @@ function createNewElements(ntask) {
 
 
 
+
     //this code sets the context in the elements 
     title.textContent = ntask.newTask
     description.textContent = ntask.description
     finishDate.textContent = `Deadline: ${ntask.deadline}`
     importance.textContent = `Priority: ${ntask.priority}`
     //this line of code appends elements into the div element
-    div.append(finishSatus, title, description, finishDate, importance);
+    div.append(finishSatus, title, deleteTask, description, finishDate, importance);
 
     //this code selects the Priority div
     const highPriorityTask = document.querySelector(".highPriorityTask");
@@ -52,6 +65,12 @@ function createNewElements(ntask) {
 
 
 }
+document.querySelectorAll(".delete_btn").forEach((button) => {
+    button.addEventListener("click", () => {
+        button.closest(".task").remove()
+        console.log(button.closest)
+    })
+})
 
 function saveTask(ntask) {
     let createdTask =
@@ -59,7 +78,8 @@ function saveTask(ntask) {
         newtitle: ntask.newTask,
         description: ntask.description,
         deadline: ntask.deadline,
-        newPriority: ntask.priority
+        newPriority: ntask.priority,
+        id: ntask.taskId
     }
     const savedData = localStorage.getItem("addedTask") // created a var to check the existing values of user data 
     if (savedData) { // checking if data already exists
@@ -90,11 +110,20 @@ document.querySelector(".submit_btn").addEventListener("click", (e) => {
     const newdeadline = document.getElementById("Deadline");
     const newpriority = document.getElementById("Priority");
 
+    // this code generates a unique number
+    const uniqueId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+
+    })
+
     const ntask = {
         newTask: Task.value,
         description: TaskDescription.value,
         deadline: newdeadline.value,
-        priority: newpriority.value
+        priority: newpriority.value,
+        taskId: uniqueId
     }
     if (Task.value.trim() === "") {
         alert("Task title cannot be empty");
@@ -110,14 +139,29 @@ document.querySelector(".submit_btn").addEventListener("click", (e) => {
 
 window.addEventListener("DOMContentLoaded", () => {//Takes data from local storage to display on screen on reload
     const addExistingTask = JSON.parse(localStorage.getItem("addedTask"));
-    addExistingTask.forEach(e => {
-        const task = {
-            newTask: e.newtitle,
-            description: e.description,
-            deadline: e.deadline,
-            priority: e.newPriority
-        }
-        createNewElements(task)
+    if (addExistingTask) {
+        addExistingTask.forEach(e => {
+            const task = {
+                newTask: e.newtitle,
+                description: e.description,
+                deadline: e.deadline,
+                priority: e.newPriority,
+                taskId: e.id
+            }
+            createNewElements(task)
 
-    });
+        });
+    }
 })
+
+document.addEventListener("click", (e) => { // this code helps to delete an item from local storage 
+    if (e.target.classList.contains("delete_btn")) {
+        let num = e.target.closest(".task")
+        let id = e.target.dataset.id
+        num.remove()
+        let existingData = JSON.parse(localStorage.getItem("addedTask"))
+        const filteredData = existingData.filter(data => data.id !== id)
+        localStorage.setItem('addedTask',JSON.stringify(filteredData))
+    }
+}
+)
